7f95f9cf3a30feb780119354247447f68cb40326
canikwe <c.anikwe@gmail.com>
2019-04-16 16:56:09 -0400

    add sort functionality for contacts

--- src/containers/ContactCardContainer.js
+++ src/containers/ContactCardContainer.js
@@ -1,17 +1,51 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import ContactCard from '../components/ContactCard'
-// import { Grid } from 'semantic-ui-react'
+import {
+  Checkbox
+} from 'semantic-ui-react'
 
-const ContactCardContainer = props => {
-  return(
+class ContactCardContainer extends React.PureComponent {
+  state = { checked: false }
+  toggle = () => this.setState({ checked: !this.state.checked })
+
+  filteredContacts = () => {
+    return this.props.contacts.filter(c => c.name.toLowerCase().includes(this.props.searchTerm.toLowerCase()))
+  }
+
+  render() {
     
-    <div>
-      <div className='ui four stackable cards'>
-        {props.contacts.filter(c => c.name.toLowerCase().includes(props.searchTerm.toLowerCase())).map(c => <ContactCard key={c.id} contact={c} />)}
+    return(
+      <div>
+        <Checkbox 
+          slider
+          label='Sort By Name'
+          checked={this.state.checked}
+          onChange={this.toggle}
+        />
+        <p />
+        <div className='ui four stackable cards'>
+          {this.state.checked ?
+            this.filteredContacts().sort((a, b) => {
+              const nameA = a.name
+              const nameB = b.name
+
+              if (nameA < nameB) {
+                return -1
+              }
+              if (nameA > nameB) {
+                return 1
+              }
+              return 0
+            }).map(c => <ContactCard key={c.id} contact={c} />)
+          :
+            this.filteredContacts().map(c => <ContactCard key={c.id} contact={c} />)
+
+          }
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 const mapStateToProps = state => {
