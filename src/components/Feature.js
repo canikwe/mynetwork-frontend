98d63de1055667c8ba698a8b7d9607e6787a4d3a
canikwe <c.anikwe@gmail.com>
2020-05-08 23:21:59 -0400

    remove unused code from Feature

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,55 +1,22 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { Grid, Label } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import '../App.css'
 import Logo from './Logo'
 import RemindersList from './RemindersList'
 
-class Feature extends React.Component {
-  constructor(){
-    super()
-    this.state = {
-      toggleReminders: false,
-    }
-  }
+const Feature = () => {
 
-  toggleReminders = () => this.setState({toggleReminders: !this.state.toggleReminders})
+  return (
+    <Grid celled='internally' stackable columns='equal'>
+      <Grid.Column>
+        <Logo />
+      </Grid.Column>
 
-  todaysReminders = () => {
-    if (this.state.sortByPriority) {
-      return this.props.reminders.filter(r => r.match).sort((a, b) => a.priority - b.priority)
-    } else {
-      return this.props.reminders.filter(r => r.match)
-    }
-  }
-  
-  render(){
-    return (
-      <Grid celled='internally' stackable columns='equal'>
-        <Grid.Column>
-            {this.todaysReminders().length === 0 || this.state.toggleReminders ?
-            null
-            : 
-            <Label as='a' color='red' ribbon='right' onClick={this.toggleReminders}>
-              {this.todaysReminders().length} Reminder(s)
-            </Label> }
-            <Logo />
-        </Grid.Column>
-        {this.state.toggleReminders ? 
-        <Grid.Column width={6}>
-          <RemindersList />
-        </Grid.Column>
-        : null
-        }
-      </Grid>)
-  }
+      <Grid.Column width={6}>
+        <RemindersList />
+      </Grid.Column>
+    </Grid>
+  )
 }
 
-const mapStateToProps = state => {
-  return {
-    user: state.user,
-    reminders: state.reminders
-  }
-}
-
-export default connect(mapStateToProps)(Feature)
+export default Feature
